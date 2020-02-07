3d120fb67b1bbb0494aacfb0c823a56e949b7058
Getachew Mulat <gech4me@gmail.com>
2020-02-07 15:26:00 +0300

    database connection using flywaydb

---
+++ src/main/java/com/example/demo/config/PostgresDatasource.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDatasource {
+
+    @Bean
+    @ConfigurationProperties("config.postgresql")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
